4d1a64a3ed5430af4f6d7dd9eb3a0371d9e17241
Stanley Hayford <hayfordstanley@gmail.com>
2019-09-09 15:45:52 +0000

    Initial Message

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Typography} from "@material-ui/core";
+
+function IndexPage(props){
+    return (
+        <div>
+            <Typography variant="h1">
+                Index Page
+            </Typography>
+        </div>
+    )
+}
+
+export default IndexPage;
